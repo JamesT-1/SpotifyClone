bbee0fd815ce7147da9b2a27b76aeea98c173430
JamesT-1 <taxmatzidis1@gmail.com>
2026-01-28 14:23:16 +0200

    Completed Project

---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,27 @@
+import { clerkClient } from "@clerk/express";
+
+export const protectRoute = async (req, res, next) => {
+  if (!req.auth.userId) {
+    return res
+      .status(401)
+      .json({ message: "Unauthorized You Must Be Logged In" });
+  }
+  next();
+};
+
+export const requireAdmin = async (req, res, next) => {
+  try {
+    const currentUser = await clerkClient.users.getUser(req.auth.userId);
+    const isAdmin =
+      process.env.ADMIN_EMAIL === currentUser.primaryEmailAddress.emailAddress;
+
+    if (!isAdmin) {
+      return res
+        .status(403)
+        .json({ message: "Unathorized You Must Be An Admin" });
+    }
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
